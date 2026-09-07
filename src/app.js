6a2b350c7d8acbcaffd47f50385afa4442337518
unknown <priyanshu8-svg>
2026-09-07 15:39:12 +0530

    Frontend url fixed

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: import.meta.env.VITE_FRONTEND_URL,
     credentials: true
 }));
 
