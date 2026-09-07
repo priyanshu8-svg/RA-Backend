da30b4b93cb19dca997ba1ca6b31f4c8c35bb704
unknown <priyanshu8-svg>
2026-09-07 16:33:25 +0530

    frontend fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 const app = express();
 
 
